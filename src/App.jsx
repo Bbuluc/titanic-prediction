c61e2c892a01baf1420ce48707e327f178796e0d
Bbuluc <baranbuluc@gmail.com>
2025-10-14 16:50:19 +0200

    Initial commit - React + Tailwind + backend setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-center'>This project is going to be about titanic...
+      </h1>
+    </>
+  )
+}
+
+export default App
